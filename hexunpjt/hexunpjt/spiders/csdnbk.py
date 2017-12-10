25015250b067ef2480718df41890331b659ed052
陈克浩 <2601516357@qq.com>
2017-12-10 15:21:38 +0800

    博客文章爬虫
    刚好碰上网站页面改版，代码修改适应网站，并增加博客主页访问量、积分等信息爬取

--- hexunpjt/hexunpjt/spiders/csdnbk.py
+++ hexunpjt/hexunpjt/spiders/csdnbk.py
@@ -1,44 +1,47 @@
-# -*- coding: utf-8 -*-
 import scrapy
 from scrapy.http import Request
 from hexunpjt.items import HexunpjtItem
-
-
-class CsdnbkSpider(scrapy.Spider):
-    name = 'csdnbk'
+import urllib.request
+class CsdnbksSpider(scrapy.Spider):
+    name = 'csdnbks'
     allowed_domains = ['csdn.net']
     wid='csdnnews'
     def start_requests(self):
         yield Request('http://blog.csdn.net/'+str(self.wid),headers=
-    {'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-   'Accept-Language':'zh-CN,zh;q=0.8',
-    'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
-   'Connection':'keep-alive'
-   })
+        {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+})
+
 
     def parse(self, response):
         item=HexunpjtItem()
-        t=response.xpath('//*[@id="article_list"]/div/div[1]/h1/span/a/text()').extract()
-        ie=[]
-        for i in range(len(t)):
-            i1=str(t[i])
-            ie.append(i1.strip())
-        item['name']=ie
-        item['url']=response.xpath('//*[@id="article_list"]/div/div[1]/h1/span/a/@href').extract()
-        item['hits']=response.xpath('//*[@id="article_list"]/div/div[3]/span[2]/text()').extract()
-        item['comment']=response.xpath('//*[@id="article_list"]/div/div[3]/span[3]/text()').extract()
-        ye=response.xpath('//*[@id="papelist"]/span/text()').extract()
-        ye1=response.xpath('//*[@id="papelist"]/strong/text()').extract()
-        yield item
-        ye=ye[0]
-        if ye1[0]=='1':
-            print('共计'+ye[0:4]+'条')
-        ye=ye[8]
-        #print(type(ye) )
-        if ye >'1':
-            for j in range(2,int(ye)+1):
-                yield Request(url='http://blog.csdn.net/csdnnews/article/list/'+str(j),callback=self.parse,
-                          headers={'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'})
+        #爬取博客主页信息
+        a=response.xpath('//*[@id="aside"]/div[2]/div[3]/div/img/@src').extract()
+        if len(a)==1:
+            a=a[0]
+            item['dj']=a[-9:-4]
+            item['fwl']=response.xpath('//*[@id="aside"]/div[2]/div[2]/span[2]/text()').extract()
+            item['pm']=response.xpath('//*[@id="aside"]/div[2]/div[4]/span[2]/text()').extract()
+            item['jf']=response.xpath('//*[@id="aside"]/div[2]/div[5]/span[2]/text()').extract()
+        #文章爬取循环
+        for j in range(1,100):
+            #网站是动态加载的，先捕捉到动态请求网址，然后验证页面是否有数据，有的话就爬取，没有就停止
+            headers2=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36')]
+            op=urllib.request.build_opener()
+            op.addheaders =headers2
+            urllib.request.install_opener(op)#将op安装为全局
+            data=urllib.request.urlopen('http://blog.csdn.net/csdnnews/svc/getarticles?pageindex='+str(j)+'&pagesize=100' ).read()
+            if len(data) !=0:
+                yield Request(url='http://blog.csdn.net/'+str(self.wid)+'/svc/getarticles?pageindex='+str(j)+'&pagesize=100',
+                            headers={'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'})
+                item['name']=response.xpath('/html/body/li/h3/a/text()').extract()
+                item['url']=response.xpath('/html/body/li/h3/a/@href').extract()
+                item['hits']=response.xpath('/html/body/li/div/div/div[3]/span/text()').extract()
+                item['comment']=response.xpath('/html/body/li/div/div/div[4]/span/text()').extract()
+                i=len(item['name'])
+            else:
+                if i!=0:print('第'+'共计'+str(i)+'篇文章')
+                break
 
+        yield item
 
 
